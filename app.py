68b77fc734571eeb6603b463b057cca812509fae
deepak001-cloud <144651562+deepak001-cloud@users.noreply.github.com>
2026-02-17 21:03:55 +0530

    Update greeting message in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello ! Jenkins + Docker Working 🚀"
+    return "Hello deepak! Jenkins + Docker is succesfully Working 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
